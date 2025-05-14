ebe632115a8c580dfa9c169438cd6c8a9706e79e
SahulGill <sahulshohamgill14@gmail.com>
2025-05-14 21:24:32 +0500

    Video#4 inventory UI setup completed

---
+++ inventory/inv_ui_slot.gd
@@ -0,0 +1,12 @@
+extends Panel
+
+@onready var item_visual: Sprite2D = $CenterContainer/Panel/item_display
+
+
+func update(Item: InvItem):
+	if !Item:
+		item_visual.visible = false
+	else:
+		item_visual.visible = true
+		item_visual.texture = Item.texture
+		
